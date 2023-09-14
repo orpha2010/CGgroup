dfed2a232483380716047269872da3be9c0e30b0
vfx震 <orpha2010@qq.com>
2023-09-15 00:20:15 +0800

    修改配置

--- source/conf.py
+++ source/conf.py
@@ -14,8 +14,8 @@ release = 'v1.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
-
+# extensions = ["myst_parser"]
+extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
